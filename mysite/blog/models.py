f5bf4c21e2b765195d95e13987941b45c9d4715c
Gabryel Mason-Williams <13510038+GMW99@users.noreply.github.com>
2020-05-28 19:17:10 +0100

    Canonical URLs for models

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 # Create your models here.
 
 # PublishedManager only shows published posts.
@@ -44,6 +45,12 @@ class Post(models.Model):
   # __str__ is the default humam-readable reprentation of the object.
   def __str__(self):
     return self.title
+  def get_absolute_url(self):
+    return reverse('blog:post_detail',
+                    args= [self.publish.year,
+                            self.publish.month,
+                            self.publish.day,
+                            self.slug])
 
 
 
