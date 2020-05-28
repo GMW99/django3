83827786a583fa17f6d3c8efc5d3939a83679005
Gabryel Mason-Williams <13510038+GMW99@users.noreply.github.com>
2020-05-28 18:40:22 +0100

    A view update
    Returning a post given date + post details

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,4 +6,15 @@ def post_list(request):
   return render(request,
                 'blog/post/list.html',
                 {'posts': posts})
+# Returns a published post, given year, month, day and post
+def post_detail(request, year, month, day, post):
+  post = get_object_or_404(Post, slug=post,
+                                status='published',
+                                publish__year=year,
+                                publish__month=month,
+                                publish__day=day)
+  return render(request,
+                'blog/post/detail.html',
+                'post',{post})
+
 # Create your views here.
