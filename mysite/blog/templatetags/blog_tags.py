ad34e9a78810f9e9f3e2d7062856f53360368526
Gabryel Mason-Williams <13510038+GMW99@users.noreply.github.com>
2020-06-06 20:30:24 +0100

    Latest Posts
    Template to show latest posts

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -5,3 +5,9 @@ register = template.Library()
 @register.simple_tag
 def total_posts():
     return Post.published.count()
+
+# This speficies the template will be rendered the returned values using blog/post/latest_posts.html
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=2):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
