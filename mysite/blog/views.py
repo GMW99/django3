8a0d6ab190e95dd1a1e2b501f3269e89ae87506a
Gabryel Mason-Williams <13510038+GMW99@users.noreply.github.com>
2020-06-06 14:25:47 +0100

    update view ORM
    Added similar post functionality.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,8 +3,9 @@ from django.core.paginator import Paginator, EmptyPage,\
                                   PageNotAnInteger
 from django.core.mail import send_mail
 from django.views.generic import ListView
-from .models import Post, Comment
+from django.db.models import Count
 from taggit.models import Tag
+from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 
 """
@@ -76,12 +77,18 @@ def post_detail(request, year, month, day, post):
       new_comment.save()
   else:
     comment_form = CommentForm()
+  post_tags_ids = post.tags.values_list('id', flat=True)
+  similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                .exclude(id=post.id)
+  similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                                .order_by('-same_tags','-publish')[:4]
   return render(request,
                 'blog/post/detail.html',
                 {'post':post,
                  'comments': comments,
                  'new_comment': new_comment,
-                 'comment_form': comment_form})
+                 'comment_form': comment_form,
+                 'similar_posts': similar_posts})
 
 # Class based list view, this is analogous with def post_list
 class PostListView(ListView):
