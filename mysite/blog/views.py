438ec41f0694970fea59682e5c4e103078eea53c
Gabryel Mason-Williams <13510038+GMW99@users.noreply.github.com>
2020-06-06 13:36:59 +0100

    Updated View
    added the ablity to commet on the view.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,8 +3,8 @@ from django.core.paginator import Paginator, EmptyPage,\
                                   PageNotAnInteger
 from django.core.mail import send_mail
 from django.views.generic import ListView
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 """
 This view works as follows:
@@ -59,9 +59,22 @@ def post_detail(request, year, month, day, post):
                                 publish__year=year,
                                 publish__month=month,
                                 publish__day=day)
+  comments = post.comments.filter(active=True)
+  new_comment = None
+  if request.method == 'POST':
+    comment_form = CommentForm(data=request.POST)
+    if comment_form.is_valid:
+      new_comment = comment_form.save(commit=False)
+      new_comment.post = post
+      new_comment.save()
+  else:
+    comment_form = CommentForm()
   return render(request,
                 'blog/post/detail.html',
-                {'post':post})
+                {'post':post,
+                 'comments': comments,
+                 'new_comment': new_comment,
+                 'comment_form': comment_form})
 
 # Class based list view, this is analogous with def post_list
 class PostListView(ListView):
